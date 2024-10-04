64a5da057fc02ee5d1fa8d31e66f8648dc0b5213
CBRA-Mirko <m.pixner@cbra.digital>
2024-10-04 09:42:13 +0200

    initial setup

---
+++ comp-test.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Composer - Test Plugin
+ * Plugin URI: https://www.cbra.digital
+ * Description: A GitHub-hosted plugin to be installed via composer.
+ * Version: 1.0
+ * Author: CBRA Digital
+ * Author URI: https://www.cbra.digital
+ */
